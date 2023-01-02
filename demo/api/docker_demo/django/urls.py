86c3b101a848f2a81c2a78a0a2f1a01ab9fccc69
Rémy Sanchez <remy.sanchez@hyperthese.net>
2023-01-02 17:23:48 +0100

    Update the demo part of the repo

--- demo/api/docker_demo/django/urls.py
+++ demo/api/docker_demo/django/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
-from docker_demo.people.views import MeViewSet
+from docker_demo.apps.people.views import MeViewSet
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 admin.site.site_title = _("Docker Demo")
