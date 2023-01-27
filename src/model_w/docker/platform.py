6dff7c63c4fd32836e2cbda2f56e28e8fbac6378
Rémy Sanchez <remy.sanchez@hyperthese.net>
2023-01-27 15:47:09 +0100

    Improve Docker detection

--- src/model_w/docker/platform.py
+++ src/model_w/docker/platform.py
@@ -1,7 +1,48 @@
 from psutil import disk_partitions
 
+__all__ = ["in_docker"]
 
-def in_docker():
+
+def _has_dns_mounts():
+    """
+    Docker will mount the DNS-related files from the host into the container.
+    If we detect this pattern, we consider that we're inside a Docker
+    container.
+    """
+
+    expected_mount_points = {"/etc/resolv.conf", "/etc/hosts", "/etc/hostname"}
+    found_mount_points = set(x.mountpoint for x in disk_partitions(all=True))
+
+    return expected_mount_points.issubset(found_mount_points)
+
+
+def _has_overlay_root():
+    """
+    In most cases, Docker will mount the root filesystem as an overlayfs. We go
+    through the list of mounts and check if the root is an overlayfs.
+    """
+
+    for part in disk_partitions(all=True):
+        if part.mountpoint == "/":
+            return part.fstype == "overlay"
+
+    return False
+
+
+def _docker_in_cgroup():
+    """
+    We're looking for the word "docker" in the cgroup file of the first process
+    as this could be an indication of being in a Docker container.
+    """
+
+    try:
+        with open("/proc/1/cgroup") as f:
+            return "docker" in f.read()
+    except FileNotFoundError:
+        return False
+
+
+def in_docker() -> bool:
     """
     Checks if we are running in a Docker container (well, not 100% sure but
     let's say it's a good enough guess).
@@ -9,16 +50,12 @@ def in_docker():
     Notes
     -----
     There is no official way to check if we are running in a Docker container.
-    What makes the most sense that I've found is that Docker will mount the
-    DNS-related files from the host into the container. So we check if those
-    files are mounted (no native system should have those mounted).
-
-    Previously we checked if the root is an overlayfs, but that's not a good
-    enough check because it's possible to run Docker with other backends
-    like ZFS.
+    This is a best-effort approach that should work in most cases. We use the
+    different methods listed above.
     """
 
-    expected_mount_points = {"/etc/resolv.conf", "/etc/hosts", "/etc/hostname"}
-    found_mount_points = set(x.mountpoint for x in disk_partitions(all=True))
+    for test in [_has_dns_mounts, _has_overlay_root, _docker_in_cgroup]:
+        if test():
+            return True
 
-    return expected_mount_points.issubset(found_mount_points)
+    return False
