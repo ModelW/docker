3fa18ec76a11a3c8d47f667fbe57be845ae1601a
Laurent Tréguier <laurent.treguier@with-madrid.com>
2023-05-03 15:59:30 +0200

    Update demo project

--- demo/front/nuxt.config.js
+++ demo/front/nuxt.config.js
@@ -1,83 +1,18 @@
-/**
- * Modifies the request from the proxy in order to make sure that Django behind
- * the request can interpret the correct host from X-Forwarded-Host instead of
- * using the host it receives which is the internal host name from the DO PaaS
- * (or any other internal name on another Kubernetes-like platform).
- */
-function addForwardedHost(proxyReq, req) {
-    const host = req.headers["x-forwarded-host"] || req.headers.host;
+import { envManager, defineModelWConfig } from "@model-w/preset-nuxt3";
 
-    if (host) {
-        proxyReq.setHeader("x-forwarded-host", host);
-    }
-}
-
-/**
- * This function decides if a request should be proxied to the API or not. Which
- * is:
- *
- * - When the request has the X-Reach-API header (aka the front-end is trying to
- *   directly reach the API)
- * - When the request is directed at /back
- *
- * An optimization in production is to configure the load balancer to always
- * send requests targeting /back to the API.
- */
-function getFromApi(path, req) {
-    if (req.headers["x-reach-api"]) {
-        return true;
-    }
-
-    const prefixes = ["back"].join("|");
-
-    if (!path.match(new RegExp(`^/(${prefixes})(/|$)`))) {
-        return false;
-    }
-
-    return true;
-}
-
-export default {
-    privateRuntimeConfig: {
-        apiUrl: process.env.API_URL,
-    },
-
-    head: {
-        titleTemplate: "%s — Docker Demo",
-        meta: [
-            { charset: "utf-8" },
-            {
-                name: "viewport",
-                content: "width=device-width, initial-scale=1",
-            },
-            { hid: "description", name: "description", content: "" },
-            { name: "format-detection", content: "telephone=no" },
-        ],
-    },
-
-    build: {
-        extend(config) {
-            config.resolve.alias.vue = "vue/dist/vue.common";
+export default envManager((env) => {
+    return defineModelWConfig(env, {
+        siteName: "docker_demo",
+        head: {
+            meta: [
+                { charset: "utf-8" },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+                { name: "format-detection", content: "telephone=no" },
+            ],
         },
-    },
-
-    buildModules: ["@nuxtjs/eslint-module"],
-
-    modules: ["@nuxtjs/proxy", "@nuxtjs/axios", "@nuxtjs/sentry", "vlang/nuxt"],
-
-    plugins: ["~/plugins/axios"],
-
-    axios: {
-        proxy: true,
-    },
-
-    proxy: [
-        [
-            (path, req) => getFromApi(path, req),
-            {
-                target: process.env.API_URL,
-                onProxyReq: addForwardedHost,
-            },
-        ],
-    ],
-};
+        cmsAlias: "",
+    });
+});
